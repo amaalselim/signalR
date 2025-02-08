abd14f3499d6745568a2907bf5b68537642d4430
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-08 22:42:41 +0200

    create Hubs

---
+++ signalRD1Smart/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace signalRD1Smart.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name,string messgae)
+        {
+            //save in DB
+            //send to all online clients
+            Clients.All.SendAsync("New Message",name, messgae);    
+
+            //send to others obline clients except me
+           // Clients.Others.SendAsync("New Message",name,messgae);   
+        }
+    }
+}
