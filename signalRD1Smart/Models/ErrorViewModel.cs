a3897f0851141faddb5d857ca85931f43f177001
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-08 22:00:11 +0200

    Add project files.

---
+++ signalRD1Smart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace signalRD1Smart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
