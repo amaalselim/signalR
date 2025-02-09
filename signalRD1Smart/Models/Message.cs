78ea2427945a208f47c2252966b80d0073956919
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-09 02:56:44 +0200

    add message model

---
+++ signalRD1Smart/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace signalRD1Smart.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Sender{ get; set; }
+        public string Content { get; set; }
+        public DateTime DateTime { get; set; }= DateTime.Now;
+
+    }
+}
