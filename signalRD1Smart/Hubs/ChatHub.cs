b7cf0598a16aea094c93dddff4288c9a21232cd0
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-09 02:56:21 +0200

    make method task and save in Db

--- signalRD1Smart/Hubs/ChatHub.cs
+++ signalRD1Smart/Hubs/ChatHub.cs
@@ -1,17 +1,37 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using signalRD1Smart.Context;
+using signalRD1Smart.Models;
 
 namespace signalRD1Smart.Hubs
 {
     public class ChatHub : Hub
     {
-        public void SendMessage(string name,string messgae)
-        {
-            //save in DB
-            //send to all online clients
-            Clients.All.SendAsync("New Message",name, messgae);    
+        private readonly ApplicationDbContext _context;
 
-            //send to others obline clients except me
-           // Clients.Others.SendAsync("New Message",name,messgae);   
+        public ChatHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task SendMessage(string name,string message)
+        {
+            try
+            {
+                //save in DB
+                var newMessage = new Message { Sender = name, Content= message };
+                _context.Messages.Add(newMessage);
+                await _context.SaveChangesAsync();
+
+                //send to all online clients
+                await Clients.All.SendAsync("NewMessage", name, message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in ChatHub: {ex.Message}");
+            }
+           
+
+            //send to others online clients except me
+           // Clients.Others.SendAsync("NewMessage",name,message);   
         }
     }
 }
