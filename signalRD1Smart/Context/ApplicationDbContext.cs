0f5e024996ced46f733267dceafbc34f4c8f5f8a
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-09 02:55:36 +0200

    add dbset of message

---
+++ signalRD1Smart/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using signalRD1Smart.Models;
+
+namespace signalRD1Smart.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){ }
+        public DbSet<Message> Messages { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=RealtimeChat;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+        }
+    }
+}
