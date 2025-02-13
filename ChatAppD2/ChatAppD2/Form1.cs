07e071b106d45dacabde70437bb7cb702b0c8ac1
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-13 03:03:25 +0200

    form a chat

--- ChatAppD2/ChatAppD2/Form1.cs
+++ ChatAppD2/ChatAppD2/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.AspNet.SignalR.Client;  
 
 namespace ChatAppD2
 {
@@ -16,20 +18,23 @@ namespace ChatAppD2
         {
             InitializeComponent();
         }
-
+        IHubProxy _proxy;
         private void Form1_Load(object sender, EventArgs e)
         {
+            HubConnection con= new HubConnection("https://localhost:44394/");
+            _proxy= con.CreateHubProxy("ChatHub");
+            con.Start();
+
+            _proxy.On<data>("NewMessage",(m)=>lb_message.Invoke(new Action(() => lb_message.Items.Add(m.Sender + ":" + m.Content))));
+            _proxy.On<string,string>("NewMember",(n,g)=> lb_message.Invoke(new Action(() => lb_message.Items.Add(n + " joinned " + g))));
+            _proxy.On<string,string,string>("groupmsg",(n,g,m)=> lb_message.Invoke(new Action(() => lb_message.Items.Add(n + " in " + g + " : " + m))));    
+
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_send_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            _proxy.Invoke("SendMessage",new data() {Sender=txt_name.Text,Content=text_message.Text});
         }
     }
 }
