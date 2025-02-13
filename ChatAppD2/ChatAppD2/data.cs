2c88320ccd84d46adc9765618693509dae26d031
Amaal Selim <141284662+amaalselim@users.noreply.github.com>
2025-02-13 03:03:09 +0200

    add model for messages

---
+++ ChatAppD2/ChatAppD2/data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAppD2
+{
+    class data
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Content { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+}
